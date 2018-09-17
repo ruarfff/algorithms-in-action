4173239404a9b51868078c711f08dd222de1941b
Ruairi O Brien <r.obrien@dell.com>
2018-09-17 16:55:15 +0100

    Added selection sort

---
+++ selectionSort.js
@@ -0,0 +1,21 @@
+exports.sort = (list) => {
+    let sorted = []
+    let listCp = [...list]
+    for(let i = 0; i < list.length; i++) {
+        let smallest = findSmallest(listCp)
+        sorted.push(listCp.splice(smallest, 1)[0])
+    }
+    return sorted
+}
+
+function findSmallest(list) {
+    let smallest = list[0]
+    let smallestIndex = 0
+    for (let i = 1; i < list.length; i++) {
+        if(list[i] < smallest) {
+            smallest = list[i]
+            smallestIndex = i
+        }
+    }
+    return smallestIndex
+}
---
+++ selectionSort.test.js
@@ -0,0 +1,5 @@
+const {sort} = require('./selectionSort');
+
+test('can sort array', () => {
+  expect(sort([0, 11, 3, 22, 5, 1])).toEqual([0, 1, 3, 5, 11, 22]);  
+});
