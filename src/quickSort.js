6a4bc446dbf511946acf1a62a08899dfcb6560cb
Ruairi O Brien <ruairitobrien@gmail.com>
2018-09-18 11:54:35 +0100

    Added test for multiple values of pivot

--- src/quickSort.js
+++ src/quickSort.js
@@ -2,7 +2,7 @@ const quickSort = list => {
   if (!list || list.length < 2) return list;
   const [pivot, ...tail] = list;
   return [
-    ...quickSort(tail.filter(val => val < pivot)),
+    ...quickSort(tail.filter(val => val <= pivot)),
     pivot,
     ...quickSort(tail.filter(val => val > pivot))
   ];
--- src/quickSort.test.js
+++ src/quickSort.test.js
@@ -4,4 +4,14 @@ test("can quickSort array", () => {
   expect(quickSort([11, 2])).toEqual([2, 11]);
   expect(quickSort([11])).toEqual([11]);
   expect(quickSort([0, 11, 3, 22, 5, 1])).toEqual([0, 1, 3, 5, 11, 22]);
+  expect(quickSort([0, 11, 3, 0, 0, 22, 5, 1])).toEqual([
+    0,
+    0,
+    0,
+    1,
+    3,
+    5,
+    11,
+    22
+  ]);
 });
