c7963f06f034f0abaf16e1ef9ad66a17676b3c26
Ruairi O Brien <ruairitobrien@gmail.com>
2018-09-19 14:25:23 +0100

    Small cleanup

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,21 +1,23 @@
-module.exports = sort = (list) => {
-    let sorted = []
-    let listCp = [...list]
-    for(let i = 0; i < list.length; i++) {
-        let smallest = findSmallest(listCp)
-        sorted.push(listCp.splice(smallest, 1)[0])
-    }
-    return sorted
-}
+const sort = list => {
+  let sorted = [];
+  let listCp = [...list];
+  for (let i = 0; i < list.length; i++) {
+    let smallest = findSmallest(listCp);
+    sorted.push(listCp.splice(smallest, 1)[0]);
+  }
+  return sorted;
+};
 
 function findSmallest(list) {
-    let smallest = list[0]
-    let smallestIndex = 0
-    for (let i = 1; i < list.length; i++) {
-        if(list[i] < smallest) {
-            smallest = list[i]
-            smallestIndex = i
-        }
+  let smallest = list[0];
+  let smallestIndex = 0;
+  for (let i = 1; i < list.length; i++) {
+    if (list[i] < smallest) {
+      smallest = list[i];
+      smallestIndex = i;
     }
-    return smallestIndex
+  }
+  return smallestIndex;
 }
+
+module.exports = sort;
